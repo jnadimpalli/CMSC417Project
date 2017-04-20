0b5a2dc8a2308b3b3110e67f36d48a8a6a21b967
jjchan1 <jjchan1@gmail.com>
2017-04-20 00:55:29 -0400

    Fixed DUMPTABLE.

--- node1.rb
+++ node1.rb
@@ -6,6 +6,7 @@ $hostname = nil
 $server = nil
 
 $nodes = {}
+$lsp = {}
 
 $updateInterval = nil
 $maxPayload = nil
@@ -67,8 +68,12 @@ def run_server
 					# for this option a client is requesting that we return info about the cost to our neighbors
 					when "COST"
 						STDERR.puts "In COST for " + $hostname
+						#STDERR.puts $nodes
 			            cost_string = ""
-			            return_node = message_info[1]
+			            return_node = message_info[1].trim
+
+						#STDERR.puts "return: " + return_node
+						#STDERR.puts "socket: " + $nodes[return_node]["SOCKET"]
 
 						STDERR.puts "Received COST request in " + $hostname + " from " + return_node
 						# return cost to all other nodes that are not the hostname
@@ -78,13 +83,15 @@ def run_server
 					        end
 						end
 
-			            cost_string += "\000"
+			            #cost_string += "\000"
 						STDERR.puts "cost_string1: " + cost_string
 
-
 			            return_socket = $nodes[return_node]["SOCKET"]
+						STDERR.puts "1"
 						return_socket.flush
-						return_socket.write(cost_string)
+						STDERR.puts "2"
+						return_socket.write(cost_string + "\000")
+						STDERR.puts "3"
 						return_socket.flush
 						STDERR.puts "Writing cost_string to socket"
 
@@ -125,6 +132,7 @@ def edgeb(cmd)
 	# connect to server and tell it who is connecting to it
 	dst_socket = TCPSocket.new(dst_ip, dst_port)
 	dst_socket.write("EDGEB #{$hostname} #{src_ip}\000")
+	#dst_socket.write("EDGEB #{$hostname} #{src_ip}\000")
 	#dst_socket.send("EDGEB #{$hostname} #{src_ip}\000", 0)
 
 	#save destination socket so that you can send messages through here later
@@ -133,75 +141,7 @@ def edgeb(cmd)
 end
 
 def dumptable(cmd)
-	stack = [$hostname]
-	visited = []
 	filename = cmd[0].split("./")[1]
-
-	# perform DFS to find all possible routes
-	while !stack.empty?
-		current_node = stack.pop
-		if !visited.include? current_node
-			visited.push(current_node)
-			STDERR.puts "Visited " + current_node
-			# if current_node is not the host, request COST message to build routing table
-			if current_node != $hostname
-				if $nodes[current_node]["SOCKET"] != nil
-					#socket = $nodes[current_node]["SOCKET"]
-					socket = TCPSocket.new($nodes[current_node]["IP"], $nodes[current_node]["PORT"])
-					#socket.send("COST #{$hostname}\000", 0)
-					socket.write("COST #{$hostname}\000")
-					STDERR.puts "COST message sent to " + current_node
-
-					# gets/recv/read do not seem to be reading the string back from the socket after using write
-					cost_string = socket.gets("\0")
-					#cost_string = socket.read()
-					#cost_string = socket.recv_nonblock($maxPayload)
-					#cost_string = socket.recv(16)
-					socket.flush
-
-					# code does not reach this point
-					STDERR.puts "cost_string2: " + cost_string
-
-					STDERR.puts "Parsing cost_string"
-					neighbors = cost_string.chomp.split(" ")
-					neighbors.each do |n|
-						node_cost = n.split(",")
-						node_neighbor = node_cost[0]
-						cost_neighbor = node_cost[1].to_i
-
-						# add children of current_node to stack for processing
-						stack.push(node_neighbor)
-
-						# if route was previously unreachable (-1) or if new route has lower cost, update cost in host's routing table
-						if ($nodes[node_neighbor]["COST"] == -1) || ($nodes[node_neighbor]["COST"] > ($nodes[current_node]["COST"] + cost_neighbor))
-							$nodes[node_neighbor]["COST"] = $nodes[current_node]["COST"] + cost_neighbor
-						end
-					end
-				end
-			else
-				# if current_node is the host, just add children since routing table is already available
-				$nodes.keys.each do |node|
-					if $nodes[node]["COST"] > 0
-						stack.push(node)
-					end
-				end
-			end
-		end
-	end
-
-
-	#since we would probably need to have an entry in the table for nodes that aren't
-	#directly connected (like if we have a node n1 connected to n2 connected to n3 we would
-	#need an entry for n3 in n1 dump table) I made some space here for a depth first (or breadth first)
-	#search to get the cost. I would recommend sending a message to the other servers and getting info back
-	#from them like this:
-
-	# for each node do this
-	# socket = $nodes[whatever node]["SOCKET"]
-	# socket.send("COST {hostname (this is because the server needs a return address)}")
-
-	#see the run_server method on what to do next
-
 
 	# for each node, check to see if a positive non-zero COST exists.
 	# If so, add to file in order: src,dst,nextHop,distance
@@ -286,7 +226,77 @@ def dijkstras(source)
 end
 
 def status()
-	STDOUT.puts "STATUS: not implemented"
+	stack = [$hostname]
+	visited = []
+	filename = cmd[0].split("./")[1]
+
+	# perform DFS to find all possible routes
+	while !stack.empty?
+		current_node = stack.pop
+		if !visited.include? current_node
+			visited.push(current_node)
+			STDERR.puts "Visited " + current_node
+			# if current_node is not the host, request COST message to build routing table
+			if current_node != $hostname
+				if $nodes[current_node]["SOCKET"] != nil
+					#socket = $nodes[current_node]["SOCKET"]
+					socket = TCPSocket.new($nodes[current_node]["IP"], $nodes[current_node]["PORT"])
+					#socket.send("COST #{$hostname}\000", 0)
+					socket.write("COST #{$hostname}\000")
+					STDERR.puts "COST message sent to " + current_node
+
+					# gets/recv/read do not seem to be reading the string back from the socket after using write
+					cost_string = socket.gets("\0")
+					#cost_string = socket.read()
+					#cost_string = socket.recv_nonblock($maxPayload)
+					#cost_string = socket.recv(16)
+					socket.flush
+
+					# code does not reach this point
+					STDERR.puts "cost_string2: " + cost_string
+
+					STDERR.puts "Parsing cost_string"
+					neighbors = cost_string.chomp.split(" ")
+					neighbors.each do |n|
+						node_cost = n.split(",")
+						node_neighbor = node_cost[0]
+						cost_neighbor = node_cost[1].to_i
+
+						# add children of current_node to stack for processing
+						stack.push(node_neighbor)
+
+						# if route was previously unreachable (-1) or if new route has lower cost, update cost in host's routing table
+						if ($nodes[node_neighbor]["COST"] == -1) || ($nodes[node_neighbor]["COST"] > ($nodes[current_node]["COST"] + cost_neighbor))
+							$nodes[node_neighbor]["COST"] = $nodes[current_node]["COST"] + cost_neighbor
+						end
+					end
+				end
+			else
+				# if current_node is the host, just add children since routing table is already available
+				$nodes.keys.each do |node|
+					if $nodes[node]["COST"] > 0
+						stack.push(node)
+					end
+				end
+			end
+		end
+	end
+
+
+	#since we would probably need to have an entry in the table for nodes that aren't
+	#directly connected (like if we have a node n1 connected to n2 connected to n3 we would
+	#need an entry for n3 in n1 dump table) I made some space here for a depth first (or breadth first)
+	#search to get the cost. I would recommend sending a message to the other servers and getting info back
+	#from them like this:
+
+	# for each node do this
+	# socket = $nodes[whatever node]["SOCKET"]
+	# socket.send("COST {hostname (this is because the server needs a return address)}")
+
+	#see the run_server method on what to do next
+
+
+
 end
 
 
@@ -353,9 +363,9 @@ def setup(hostname, port, nodes, config)
 	$port = port
 
 	# start separate server thread
-	server_thread = Thread.new do
-		run_server()
-	end
+	# server_thread = Thread.new do
+	# 	run_server()
+	# end
 
 	#set up ports, server, buffers
 	#$socketToNode = {} #Hashmap to index node by socket
@@ -370,15 +380,6 @@ def setup(hostname, port, nodes, config)
 
 		$nodes[node_name] = {}
 		$nodes[node_name]["SOCKET"] = nil
-
-		# originally thought that a TCPSocket could be opened with only a name and the system
-		# would automatically change it but cannot find it in the documentation anymore.
-		# Trying to do it while chaining COST message requests
-		if (node_name != $hostname)
-			#STDERR.puts "Setting up " + node_name + " for " + $hostname
-			#socket = TCPSocket.new("1.1.1.1", node_port)
-			#$nodes[node_name]["INFO"] = socket
-		end
 		$nodes[node_name]["PORT"] = node_port.to_i
 		# 0 is self, -1 is unreachable (infinity)
 		$nodes[node_name]["COST"] = -1
@@ -410,9 +411,9 @@ def setup(hostname, port, nodes, config)
 
 	#STDERR.puts $nodes
 	# start separate server thread
-	#server_thread = Thread.new do
-	#	run_server()
-	#end
+	server_thread = Thread.new do
+		run_server()
+	end
 
 	main()
 
